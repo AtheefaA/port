c986a2d1cb482608d3b1a95aa5759c2214aa9383
shafa <atheefa1406@gmail.com>
2026-02-07 19:17:51 +0530

    Updated project

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,13 +20,14 @@ const Home = () => {
           <div className="home-buttons">
             {/* ✅ Updated "Hire Me" button to open Gmail compose */}
             <a
-              href="https://mail.google.com/mail/?view=cm&fs=1&to=atheefa1406@gmail.com&su=Job%20Opportunity&body=Hello%20Atheefa%2C%20I’d%20like%20to%20connect%20regarding%20a%20role."
-              target="_blank"
-              rel="noopener noreferrer"
-              className="btn hire-btn"
-            >
-              Hire Me
-            </a>
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=atheefa1406@gmail.com&su=Job Opportunity&body=Hello Atheefa, I would like to connect regarding a potential opportunity."
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn hire-btn"
+              >
+                Hire Me
+              </a>
+
 
             {/* Download CV button */}
             <a href={CV} download="Atheefa Resume.pdf" className="btn-outline">
