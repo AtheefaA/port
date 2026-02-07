43d9ecfa00c1e0093dc4a997d79d0b7bfbb5623c
shafa <atheefa1406@gmail.com>
2026-02-07 18:56:56 +0530

    Update image1 import

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Home.css";
 import CV from "../../assets/Atheefa final res.pdf";
-import Profile from "../../assets/pgit add .rofile.jpeg";
+import Profile from "../../assets/profile.jpeg";
+
 
 const Home = () => {
   return (
